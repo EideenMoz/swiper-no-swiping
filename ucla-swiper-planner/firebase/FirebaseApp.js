e3ba407be34a3ef66d76b56e7f9c93c2189aaf51
rheajain10 <rheajain@g.ucla.edu>
2024-03-08 20:40:26 -0800

    Fixed firebaseConfig
    Updated firebase Config so that there are no dangling API keys or IDs and are replaced with variables from a local .env.local file

--- ucla-swiper-planner/firebase/FirebaseApp.js
+++ ucla-swiper-planner/firebase/FirebaseApp.js
@@ -8,14 +8,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYzeDm9US8f6C1fHTul2Djwk1SFvCIzUI",
-  authDomain: "swiper-no-swiping-ca90d.firebaseapp.com",
-  databaseURL: "https://swiper-no-swiping-ca90d-default-rtdb.firebaseio.com",
-  projectId: "swiper-no-swiping-ca90d",
-  storageBucket: "swiper-no-swiping-ca90d.appspot.com",
-  messagingSenderId: "766010906720",
-  appId: "1:766010906720:web:610e75cc5f204b3d9acb05",
-  measurementId: "G-F0J4NGV2E5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
